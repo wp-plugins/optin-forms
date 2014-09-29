4ca691254ac33d521268c6822a2c720153388b8e
brs <brs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-29 09:35:01 +0000

    version 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/optin-forms/tags/1.1.3@998391 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/sidebar.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+                <div class="optinforms-ad-product">
+                    	<h4><a href="http://www.codeleon.com/riseforums" target="_blank">JOIN RISE FORUMS FOR FREE!</a></h4>
+                        	<p>Learn how to drive your business forward. Internet marketing forums with tips and tricks from other successful online entrepreneurs. <b>Learn from the best!</b></p>
+                    	<div class="optinforms-ad-product-button"><a href="http://www.codeleon.com/riseforums" target="_blank"><?php echo __('Join for free', 'optinforms'); ?></a></div>
+                </div><!--optinforms-ad-product-->
+                <div class="optinforms-ad">
+                    <ul>
+                    	<li><a href="http://wordpress.org/support/plugin/optin-forms" target="_blank"><?php echo __('Get support', 'optinforms'); ?></a></li>
+                        <li><a href="http://wordpress.org/plugins/optin-forms" target="_blank"><?php echo __('Rate our plugin on WordPress.org', 'optinforms'); ?></a></li>
+                        <li><a href="http://www.twitter.com/codeleoncom" target="_blank"><?php echo __('Follow us on Twitter', 'optinforms'); ?></a></li>
+                        <li><a href="http://www.facebook.com/codeleoncom" target="_blank"><?php echo __('Follow us on Facebook', 'optinforms'); ?></a></li>
+                        <li><a href="http://www.google.com/+Codeleon" target="_blank"><?php echo __('Follow us on Google+', 'optinforms'); ?></a></li>
+                    </ul>
+                    <p>A plugin by <a href="http://www.codeleon.com/?utm_source=OptinForms&utm_medium=sidebar&utm_campaign=logo" target="_blank"><img src="<?php echo plugins_url(); ?>/optin-forms/images/logo.png" alt="Codeleon" /></a></p>
+                </div><!--optinforms-ad-->
+
+<?php ?>
