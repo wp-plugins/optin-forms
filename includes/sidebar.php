2b902d65acd1fa643113ed6e1372996e8484b0a0
brs <brs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-13 19:38:25 +0000

    Update to 1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/optin-forms/tags/1.1.4@1025295 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/sidebar.php
@@ -0,0 +1,25 @@
+<?php ?>
+
+                <div class="optinforms-ad-product">
+                    	<h4><a href="https://optinlock.com/?utm_source=sidebar&utm_medium=link&utm_campaign=OptinForms" target="_blank">Boost Your Conversions with Optin Lock</a></h4>
+                        <p>Boost your conversion rates by locking parts of your posts and pages. Serve your premium content to subscribers only.</p>
+                        <ul>
+                            <li>Create multiple lead-magnets</li>
+                        	<li>Create unlimited optin forms</li>
+                            <li>Create unlimited lightbox popups</li>
+                            <li>Analyze built-in performance statistics</li>
+                        </ul>
+                    	<div class="optinforms-ad-product-button"><a href="https://optinlock.com/?utm_source=sidebar&utm_medium=button&utm_campaign=OptinForms" target="_blank">Get Optin Lock Now!</a></div>
+                </div><!--optinforms-ad-product-->
+                <div class="optinforms-ad">
+                    <ul>
+                    	<li><a href="http://wordpress.org/support/plugin/optin-forms" target="_blank"><?php echo __('Get support', 'optinforms'); ?></a></li>
+                        <li><a href="http://wordpress.org/plugins/optin-forms" target="_blank"><?php echo __('Rate our plugin on WordPress.org', 'optinforms'); ?></a></li>
+                        <li><a href="http://www.twitter.com/codeleoncom" target="_blank"><?php echo __('Follow us on Twitter', 'optinforms'); ?></a></li>
+                        <li><a href="http://www.facebook.com/codeleoncom" target="_blank"><?php echo __('Follow us on Facebook', 'optinforms'); ?></a></li>
+                        <li><a href="http://www.google.com/+Codeleon" target="_blank"><?php echo __('Follow us on Google+', 'optinforms'); ?></a></li>
+                    </ul>
+                    <p>A plugin by <a href="http://www.codeleon.com/?utm_source=sidebar&utm_medium=logo&utm_campaign=OptinForms" target="_blank"><img src="<?php echo plugins_url(); ?>/optin-forms/images/logo.png" alt="Codeleon" /></a></p>
+                </div><!--optinforms-ad-->
+
+<?php ?>
