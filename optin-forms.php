784bd09551b1137f82ec22a112a6acd08faadebf
brs <brs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-05 16:57:39 +0000

    new version release
    git-svn-id: https://plugins.svn.wordpress.org/optin-forms/tags/1.1.5@1128264 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ optin-forms.php
@@ -0,0 +1,249 @@
+<?php 
+/*
+Plugin Name: Optin Forms
+Plugin URI: http://www.codeleon.com/wordpress/plugins/optin-forms
+Description: Create beautiful optin forms with ease. Choose a form design, customize it, and add your form to your blog with a simple mouse-click.
+Author: Boris Beo
+Version: 1.1.5
+Author URI: http://www.codeleon.com
+Text Domain: optinforms
+Domain Path:   /languages/
+License:
+  Copyright 2015 codeleon.com
+ 
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Add translation to plugin
+function optinforms_init() {
+	$plugin_dir = basename(dirname(__FILE__));
+	load_plugin_textdomain( 'optinforms', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action('plugins_loaded', 'optinforms_init');
+
+// Allow for translation of plugin description
+$plugin_header_translate = array(
+	__('Create beautiful optin forms with ease. Choose a form design, customize it, and add your form to your blog with a simple mouse-click.', 'optinforms'),
+);
+
+// Add our menu to WordPress
+add_action( 'admin_menu', 'optinforms_menu' );
+
+function optinforms_menu() 
+{
+	// Since 1.1.2 added a menu position decimal fix to prevent conflict with other themes using 31, such as Thesis Theme
+	// @http://gabrielharper.com/blog/2012/08/wordpress-admin-menu-positioning-conflicts/
+	$submenu = add_menu_page(__('Optin Forms','menu-test'), __('Optin Forms','menu-test'), 'manage_options', 'optinforms', 'optinforms_main_page', plugins_url('optin-forms/images/icon.png'), '30.1');
+	
+	// We want our JS and CSS loaded on our admin pages only, so let's just load them for now
+	add_action( 'load-' . $submenu, 'load_optinforms_admin_scripts' );
+}
+
+// Enqueue our CSS and JS on Optin Forms admin pages only
+function load_optinforms_admin_scripts() {
+	add_action( 'admin_enqueue_scripts', 'optinforms_admin_scripts' );
+}
+
+// Include our registration settings
+include( plugin_dir_path( __FILE__ ) . 'includes/register-settings.php');
+// Include our regular functions
+include( plugin_dir_path( __FILE__ ) . 'includes/functions.php');
+// Include our form functions
+include( plugin_dir_path( __FILE__ ) . 'includes/functions-form-1.php');
+include( plugin_dir_path( __FILE__ ) . 'includes/functions-form-2.php');
+include( plugin_dir_path( __FILE__ ) . 'includes/functions-form-3.php');
+include( plugin_dir_path( __FILE__ ) . 'includes/functions-form-4.php');
+include( plugin_dir_path( __FILE__ ) . 'includes/functions-form-5.php');
+include( plugin_dir_path( __FILE__ ) . 'includes/functions-forms.php');
+
+
+// Add our CSS and JS to admin head, but just for our pages (see load_optinforms_admin_scripts above!)
+function optinforms_admin_scripts()
+{
+	wp_enqueue_style('optinforms-admin-stylesheet', plugins_url('/css/optinforms-admin.css', __FILE__ ), array('optinforms-googleFont'));
+	wp_enqueue_script('tabcontent', plugins_url('/js/tabcontent.js', __FILE__ ));
+	wp_enqueue_style('wp-color-picker');
+	wp_enqueue_script('optinforms-color', plugins_url('/js/optinforms-color.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+	wp_enqueue_script('placeholder', plugins_url('/js/placeholder.js', __FILE__ ));
+	wp_enqueue_script('toggle', plugins_url('/js/custom.js', __FILE__ ));
+	wp_enqueue_script('jquery-ui-slider');
+	wp_register_style('optinforms-googleFont', 'http://fonts.googleapis.com/css?family=Share+Tech|Droid+Sans|Lobster|Fenix|Unkempt|Flavors|Viga|Damion|Oleo+Script|Racing+Sans+One|Nixie+One|Fredoka+One|Open+Sans|Overlock+SC|Bubbler+One|Contrail+One|Gochi+Hand|Roboto+Condensed|Russo+One|Cinzel+Decorative|News+Cycle|Marcellus+SC|Chewy|Quicksand|Sanchez|Signika+Negative|Gloria+Hallelujah|Grand+Hotel|Droid+Serif|Englebert|Oswald|Pacifico|Titan+One|Shadows+Into+Light|Dancing+Script|Luckiest+Guy|Parisienne|Coming+Soon|Baumans|Belgrano');
+}
+
+// Enqueue our form CSS on front end
+	add_action( 'wp_enqueue_scripts', 'optinforms_scripts' );
+
+
+// Add our CSS and JS to admin head, but just for our pages (see load_optinforms_admin_scripts above!)
+function optinforms_scripts()
+{
+	global $optinforms_form_design;
+	wp_enqueue_script('jquery');
+	wp_enqueue_style('optinforms-stylesheet', plugins_url('/css/optinforms.css', __FILE__ ), array('optinforms-googleFont'));
+	wp_enqueue_script('placeholder', plugins_url('/js/placeholder.js', __FILE__ ));
+	wp_register_style('optinforms-googleFont', optinforms_used_fonts());
+}
+
+// Add additional scripts to admin head on all admin pages (so supportbox slider will work on all pages!)
+function optinforms_load_additional_scripts(){
+	wp_enqueue_style('optinforms-admin-slider-stylesheet', plugins_url('/css/optinforms-admin-slider.css', __FILE__ ));
+	wp_enqueue_script('jquery-ui-slider');
+}
+add_action( 'admin_enqueue_scripts', 'optinforms_load_additional_scripts' );
+
+// Make sure user can manage options
+function optinforms_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+?>
+
+<?php }
+function optinforms_main_page() {
+	
+	{ ?>
+    <div class="wrap">
+    	<h2><?php echo __('Optin Forms', 'optinforms'); ?></h2>
+    	<div id="icon-optinforms" class="icon32">
+        </div><!--icon-32-->
+		<h3 class="title"><?php echo optinforms_menu_tabs(); ?></h3>
+    </div><!--wrap-->
+    
+		<?php echo optinforms_configuration(); ?>
+
+        <?php if( isset($_GET['settings-updated']) ) { ?>
+    		<div id="message" class="updated">
+        		<p><strong><?php _e('Settings updated') ?></strong> - <?php echo __('If you like Optin Forms, you will love', 'optinforms'); ?> <a href="https://optinlock.com/?utm_source=SaveSettings&utm_medium=link&utm_campaign=OptinForms" target="_blank">Optin Lock</a></p>
+    		</div>
+		<?php } ?>
+    
+    <div id="optinforms">
+      	<form method="post" action="options.php" id="frm1">
+        <?php settings_fields( 'optinforms-settings-group' ); ?>
+    	
+        <div id="optinforms-email-solution-tab" class="tabcontent">
+        	<div class="optinforms-container-left">
+                <?php include( plugin_dir_path( __FILE__ ) . 'includes/options-email-solution.php'); ?>
+            </div><!--optinforms-container-left-->
+            <div class="optinforms-container-right">
+            	<?php include( plugin_dir_path( __FILE__ ) . 'includes/sidebar.php'); ?>
+            </div><!--optinforms-container-right-->
+            <div class="clear"></div>
+    	</div><!--optinforms-email-solution-tab-->
+        
+    	<div id="optinforms-posts-tab" class="tabcontent">
+        	<div class="optinforms-container-left">
+                <div class="optiongroup">
+                    <p><?php echo __('Add a beautiful optin form to your posts, custom post types and pages. Include the form on your website with a simple mouse-click, or use the shortcode to add it to specific posts and pages.', 'optinforms'); ?></p>
+                    <div class="optionleft">
+                        <label for="optinforms_form_design" class="nopointer"><?php echo __('Form design', 'optinforms'); ?></label>
+                    </div><!--optionleft-->
+                    <div class="optionmiddle">
+                        <select name="optinforms_form_design" id="optinforms_form_design">
+                            <option value="optinforms_form_design_option1" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option1') echo 'selected="selected"'; ?>>01</option>
+                            <option value="optinforms_form_design_option2" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option2') echo 'selected="selected"'; ?>>02</option>
+                            <option value="optinforms_form_design_option3" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option3') echo 'selected="selected"'; ?>>03</option>
+                            <option value="optinforms_form_design_option4" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option4') echo 'selected="selected"'; ?>>04</option>
+                            <option value="optinforms_form_design_option5" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option5') echo 'selected="selected"'; ?>>05</option>
+                        </select>
+                        <script type="text/javascript">
+                            document.getElementById('optinforms_form_design').onchange = function() {
+                                var i = 1;
+                                var myDiv = document.getElementById("optinforms_form_design_option" + i);
+                                while(myDiv) {
+                                    myDiv.style.display = 'none';
+                                    myDiv = document.getElementById("optinforms_form_design_option" + ++i);
+                                }
+                            document.getElementById(this.value).style.display = 'block';
+                        };
+                        </script>
+                    </div><!--optionmiddle-->
+                    <div class="optionlast">
+                                    
+                    </div><!--optionlast-->
+                    <div class="clear"></div>
+        
+                </div><!--optiongroup-->
+                
+                <div class="optiongroup">
+                    <div id="optinforms_form_design_option1" <?php if (get_option('optinforms_form_design')== '' || get_option('optinforms_form_design')== 'optinforms_form_design_option1') echo 'style="display:block;"'; ?>>
+                        
+						<?php include( plugin_dir_path( __FILE__ ) . 'includes/preview-form-1.php'); ?>
+                       	<?php include( plugin_dir_path( __FILE__ ) . 'includes/options-form-1.php'); ?>
+                    
+                    </div><!--optinforms_form_design_option1-->
+                    <div id="optinforms_form_design_option2" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option2') echo 'style="display:block;"'; ?>>
+                        
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/preview-form-2.php'); ?>
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/options-form-2.php'); ?>
+                        
+                    </div><!--optinforms_form_design_option2-->
+                    <div id="optinforms_form_design_option3" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option3') echo 'style="display:block;"'; ?>>
+                        
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/preview-form-3.php'); ?>
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/options-form-3.php'); ?>
+                        
+                    </div><!--optinforms_form_design_option3-->
+                    <div id="optinforms_form_design_option4" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option4') echo 'style="display:block;"'; ?>>
+                    
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/preview-form-4.php'); ?>
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/options-form-4.php'); ?>
+                        
+                    </div><!--optinforms_form_design_option4-->
+                    <div id="optinforms_form_design_option5" <?php if (get_option('optinforms_form_design')== 'optinforms_form_design_option5') echo 'style="display:block;"'; ?>>
+                        
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/preview-form-5.php'); ?>
+                        <?php include( plugin_dir_path( __FILE__ ) . 'includes/options-form-5.php'); ?>
+                        
+                    </div><!--optinforms_form_design_option5-->
+                </div><!--optiongroup-->
+                
+                <?php include( plugin_dir_path( __FILE__ ) . 'includes/options-form-functionality.php'); ?>
+                <?php include( plugin_dir_path( __FILE__ ) . 'includes/options-form-placement.php'); ?>
+                
+        	</div><!--optinforms-container-left-->
+            <div class="optinforms-container-right">
+            	<?php include( plugin_dir_path( __FILE__ ) . 'includes/sidebar.php'); ?>
+            </div><!--optinforms-container-right-->
+            <div class="clear"></div>
+        </div><!--optinforms-posts-tab-->
+        
+        <script type="text/javascript">
+			var wpthumbs=new ddtabcontent("optinforms-menu-tabs") //enter ID of Tab Container
+			wpthumbs.setpersist(true) //toogle persistence of the tabs' state
+			wpthumbs.setselectedClassTarget("link") //"link" or "linkparent"
+			wpthumbs.init()
+        </script>
+        
+        <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+        </form>
+        
+        <div id="emailbox">
+        	<form method="post" action="http://www.codeleon.com/send/form.php?form=5" id="frmSS5">
+        	<h3><?php echo __('Learn how to optimize your optin forms and get more subscribers right away!', 'optinforms'); ?></h3>
+    		<span><?php echo __('My email is', 'optinforms'); ?></span>
+            <input type="text" name="email" value="<?php global $current_user; get_currentuserinfo(); echo $current_user->user_email; ?>" />
+            <span><?php echo __('and my name is', 'optinforms'); ?></span>
+            <input type="text" name="CustomFields[1]" id="CustomFields_1_5" value="<?php global $current_user; get_currentuserinfo(); echo $current_user->display_name; ?>">
+            <input type="hidden" name="format" value="h" />
+            <input type="submit" class="emailbox-subscribe" value="<?php echo __('Subscribe', 'optinforms'); ?>" />
+            </form>
+    	</div><!--emailbox-->  
+        
+	</div><!--optinforms-->
+
+<?php }
+
+
+} ?>
---
+++ readme.txt
@@ -0,0 +1,113 @@
+=== Optin Forms ===
+Contributors: brs
+Tags: optin, form, forms, aweber, icontact, mailchimp, getresponse, mad mimi, interspire, email marketer, email marketing, email, e-mail, list, list building, opt-in, newsletter, autoresponder, popup, pop-up, mailing, mailing list, mail, webform, newsletter plugin, optin plugin, opt-in plugin, conversion, design
+Requires at least: 3.2
+Tested up to: 4.1.1
+Stable tag: 1.1.5
+
+Create beautiful optin forms with ease. Choose a form design, customize it, and add your form to your blog with a simple mouse-click.
+
+== Description ==
+
+**Create Beautiful Optin Forms With Ease!**
+
+Optin Forms allows you to create stunning optin forms for your blog or website, without the need to know any HTML or CSS. Simply select your email solution, pick a form design and customize its appearance. Add your form to your posts and pages with a simple mouse-click, or use a shortcode to add it to specific posts and pages.
+
+Optin Forms works with the following email solutions:
+
+* AWeber
+* iContact
+* MailChimp
+* GetResponse
+* MadMimi
+* Interspire Email Marketer
+
+**Optin Forms Comes With 5 Beautiful Form Designs!**
+
+All form designs are fully customizable. You can easily change all texts, fonts and font sizes. Change the colors of any element by selecting a new value from the color picker.
+
+**Translations**
+
+* Serbian - Thanks to Ogi from [First Site Guide](http://firstsiteguide.com/)
+* Spanish - Thanks to Andrew from [Web Hosting Hub](http://www.webhostinghub.com/)
+* Turkish - Thanks to Yigit from [Swanky Recipes](http://www.swankyrecipes.com/)
+* [Send us your translation](http://www.codeleon.com/contact)
+
+== Installation ==
+
+1. Download and unzip the plugin.
+2. Upload the `optin-forms` directory to the `wp-content/plugins/` directory.
+3. Click on 'Plugins' in the navigation menu and activate the plugin.
+4. Click on 'Optin Forms' in the navigation menu.
+
+== Frequently Asked Questions ==
+
+= How can I get support? =
+ 
+Having trouble with the plugin? Simply post your question to the [support forum](http://wordpress.org/support/plugin/optin-forms) and we'll help you out.
+
+== Screenshots ==
+
+1. Select your email solution and configure the required fields.
+2. Select a form design and customize its appearance.
+3. Hide elements under Form Options.
+3. Add your form to your website with a simple mouse-click.
+
+== Changelog ==
+
+= 1.1.5 =
+* Added MailChimp Form Action URL check.
+* Fixed SSL issue.
+* Fixed Google Fonts error issue.
+* Added pledge box.
+
+= 1.1.4 =
+* Added Serbian translation.
+* Updated AWeber instructions.
+* Updated sidebar admin.
+* Updated mailing form.
+
+= 1.1.3 =
+* Improved responsive design form 02. 
+* Added Spanish translation.
+
+= 1.1.2 =
+* Fixed responsive design forms.
+* Added option to open form submissions in a new window.
+* Fixed jQuery warning placeholder.js.
+
+= 1.1.1 =
+* Fixed exclude function.
+
+= 1.1 =
+* Added exclude options.
+* Added shortcode.
+* Added form options.
+* Added custom CSS fields.
+* Merged Placement tab with Form tab.
+
+= 1.0 =
+* First version!
+
+== Upgrade Notice ==
+
+= 1.1.5 =
+Updated MailChimp Form Action URL check, fixed SSL issues and Google Fonts error issue.
+
+= 1.1.4 =
+Updated Aweber instructions and added the Serbian translation for the plugin.
+
+= 1.1.3 =
+Fixed responsive design for the second form and added the Spanish translation for the plugin.
+
+= 1.1.2 =
+Fixed responsive design for all form designs and included the option to open form submissions in a new window.
+
+= 1.1.1 =
+Fixed exclude function that caused warnings on certain installations.
+
+= 1.1 =
+New display options, exclude your optin form on specific posts and pages, and add your form with a shortcode!
+
+= 1.0 =
+First version!
